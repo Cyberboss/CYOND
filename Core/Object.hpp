63ba532426ae509476a501f88b3c2826ba8f8098
Cyberboss <Cyberboss@users.noreply.github.com>
2016-11-04 13:04:36 -0400

    Comment out offending things

--- Core/Object.hpp
+++ Core/Object.hpp
@@ -5,6 +5,6 @@ namespace CYOND {
 	//! @brief Anything that needs to be allocated in the heap (Dynamic lists/strings, datums with unscoped life)
 	class Object {
 	private:
-		ObjectReference* FReferences;
+		//ObjectReference* FReferences;
 	};
 };
